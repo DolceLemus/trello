2591bfc757904b124e1d04df9b5108d57f50b85a
DolceLemus <dulce.lemus.lara@gmail.com>
2018-05-16 08:45:00 -0500

    VERSION 0.0.1.1 se reemplaza input pero se tiene pendiente que se permita escribir desde el primer click

--- js/app.js
+++ js/app.js
@@ -4,8 +4,13 @@ var container = document.getElementById('container');
 // VERSION 0.0.1
 addList.addEventListener('click', function() {
     var containerForm = document.getElementById('containerForm');
-
+    // input ya creado
     var inputContainer = document.getElementById('add-comment-input');
+
+    // input creado en js para que reemplace el primero
+    var inputContainerNew = document.createElement('textarea'); //nuevo input para reemplazar al original y poder escribir
+
+
     var btnAceptar = document.createElement('button');
     var cancel = document.createElement('i');
     var space = document.createElement('br');
@@ -14,7 +19,7 @@ addList.addEventListener('click', function() {
     // created elements
     btnAceptar.appendChild(btnTextContainer);
 
-    containerForm.appendChild(inputContainer);
+    containerForm.appendChild(inputContainerNew); //nuevo input para reemplazar al original y poder escribir
     containerForm.appendChild(space);
     containerForm.appendChild(btnAceptar);
     containerForm.appendChild(cancel);
@@ -27,6 +32,10 @@ addList.addEventListener('click', function() {
     btnAceptar.classList.add('btnAceptar');
     cancel.classList.add('iconCancel');
     containerForm.classList.add("containerForm");
+    inputContainerNew.id = 'add-comment-input';//nuevo input para reemplazar al original y poder escribir
+
+    inputContainer.style.display = "none";
+    inputContainerNew.placeholder = " Añadir una lista..."//nuevo input para reemplazar al original y poder escribir
 
 
     // containerForm.parentElement.insertBefore(containerForm);
